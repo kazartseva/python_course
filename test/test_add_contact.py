06606d3f67ba25d46f497ac46a9bf5f6ec067310
kazartseva <kazarceva@gmail.com>
2018-08-03 17:02:27 +0200

    Задание #5: Реализовать двухуровневую архитектуру тестов
    Созданы классы-помощники и в них перенесены вспомогательные функции

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -14,17 +14,11 @@ def app(request):
 
 def test_create_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(firstname="Name", middlename="Middle name", lastname="Last name",
+    app.contact.create(Contact(firstname="Name", middlename="Middle name", lastname="Last name",
                                nickname="Nickname", title="Title", company="Company", address="Address", home="6666666",
                                mobile="555555", work="444444", fax="1111", email="email1", email2="email2",
                                email3="email3",
                                homepage="www.homapage.com", bday="1", bmonth="March", byear="1856", aday="2",
                                amonth="May",
                                ayear="1890", address2="address", phone2="home", notes="notes"))
-    app.session.logout()
-
-
-def test_add_group(app):
-    app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="new group 5", header="header", footer="footer"))
     app.session.logout()
