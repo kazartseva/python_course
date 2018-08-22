6452c9c0be975371e126b3b6230293fc097715dd
kazartseva <kazarceva@gmail.com>
2018-08-22 14:21:37 +0200

    Тест

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -3,7 +3,7 @@ from model.contact import Contact
 
 def test_delete_first_contact(app):
     if app.contact.count() == 0:
-        app.contact.create(Contact(firstname="test firstname", lastname="test lastname", address="test Address"))
+        app.contact.create(Contact(firstname="test FIRSTNAME", lastname="test lastname", address="test Address"))
     old_contacts = app.contact.get_contacts_list()
     app.contact.delete_first_contact()
     assert len(old_contacts) - 1 == app.contact.count()
